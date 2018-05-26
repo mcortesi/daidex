88472d2ff38e7ec77899c0ecab8c15b688d3fd91
Mariano A. Cortesi <mariano@flowics.com>
2018-05-26 19:00:49 -0300

    refech orderbook every 2mins

--- src/model/widget-state/epics/orderbook.ts
+++ src/model/widget-state/epics/orderbook.ts
@@ -1,11 +1,13 @@
-import { map, switchMap } from 'rxjs/operators';
+import { map, switchMap, startWith, exhaustMap } from 'rxjs/operators';
 import { WidgetEpic } from '.';
 import { promiseFactory, select } from '../../../utils/rx';
 import { OrderBookEvent, OrderEventKind, ServerApi } from '../../server-api';
 import { orderbookEvent, Actions } from '../actions';
-import { Observable } from 'rxjs';
+import { Observable, interval } from 'rxjs';
 import { Change } from '../store';
 import { WidgetState } from '..';
+
+const POLL_INTERVAL = 2 * 60 * 1000; // 2 minutes
 
 export const orderbook = (api: ServerApi): WidgetEpic => (
   changes: Observable<Change<WidgetState, Actions>>
@@ -14,14 +16,19 @@ export const orderbook = (api: ServerApi): WidgetEpic => (
     select('state', 'tradeable'),
     // switchMap(tradeable => api.orderBookWatcher(tradeable.address)),
     switchMap(tradeable =>
-      promiseFactory(async () => {
-        const ob = await api.getOrderBook(tradeable.address);
-        return {
-          kind: OrderEventKind.Snapshot,
-          tradeableAddress: tradeable.address,
-          snapshot: ob,
-        } as OrderBookEvent;
-      })
+      interval(POLL_INTERVAL).pipe(
+        startWith(-1),
+        exhaustMap(() =>
+          promiseFactory(async () => {
+            const ob = await api.getOrderBook(tradeable.address);
+            return {
+              kind: OrderEventKind.Snapshot,
+              tradeableAddress: tradeable.address,
+              snapshot: ob,
+            } as OrderBookEvent;
+          })
+        )
+      )
     ),
     map(orderbookEvent)
   );
