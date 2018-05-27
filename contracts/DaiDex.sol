05394370d723a1d489c11e59e4833b83597e24c0
Daniel Fernandez <fernandez.daniel@gmail.com>
2018-05-26 22:54:14 -0400

    fix daidex contract

--- contracts/DaiDex.sol
+++ contracts/DaiDex.sol
@@ -7,6 +7,7 @@ contract IStandardToken {
   function transfer(address to, uint256 value) public returns (bool) {}
   function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {}
   function approve(address _spender, uint256 _value) public returns (bool) {}
+  function balanceOf(address _owner) external view returns (uint balance) {}
 }
 
 contract IWeth is IStandardToken {
@@ -17,6 +18,7 @@ contract IWeth is IStandardToken {
 contract IDaiMatchingMarket {
     function sellAllAmount(IStandardToken pay_gem, uint pay_amt, IStandardToken buy_gem, uint min_fill_amount) public returns (uint fill_amt) {}
     function buyAllAmount(IStandardToken buy_gem, uint buy_amt, IStandardToken pay_gem, uint max_fill_amount) public returns (uint fill_amt) {}
+    function getBuyAmount(IStandardToken buy_gem, IStandardToken pay_gem, uint pay_amt) public constant returns (uint fill_amt) {}
 }
 
 contract IDexdex {
@@ -51,6 +53,7 @@ contract DaiDex /*is Migratable*/ {
       weth = IWeth(wethAddress);
     }
 
+  function() public payable {}
 
   /*
    * Buys ECR20 Token using DAI, only if the exchanged amount of DAI to ETH doesn't exceed a given ETH amount.
@@ -58,10 +61,12 @@ contract DaiDex /*is Migratable*/ {
   function buy(IStandardToken tokenToBuy, uint volumeTokenToBuy, uint volumeDai, uint volumeEth, bytes ordersData) {
     // Obtain Dai From Sender. Sender previously executed a transaction approving Dai transfer to this contract.
     require(dai.transferFrom(msg.sender, this, volumeDai));
-
     dai.approve(address(daiMatchingMarket), volumeDai);
-    uint volumeWethEffective = daiMatchingMarket.sellAllAmount(dai, volumeDai, weth, volumeEth);
-    require(volumeWethEffective >= volumeEth, "Effective Eth Volume is lower than expected");
+
+    uint volumeWethEffective = daiMatchingMarket.getBuyAmount(weth, dai, volumeDai);
+
+    daiMatchingMarket.buyAllAmount(weth, volumeWethEffective, dai, volumeDai);
+    //require(volumeWethEffective >= volumeEth, "Effective Eth Volume is lower than expected");
     weth.withdraw(volumeWethEffective);
 
     dexdex.buy.value(volumeWethEffective)(tokenToBuy, volumeTokenToBuy, ordersData, msg.sender, address(0));
